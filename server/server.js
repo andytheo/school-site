35ff7741608377780cce306ab015c648580b3aeb
andytheo <igbeinkutuandrew@yahoo.com>
2022-01-09 15:57:38 -0500

    package

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use("/api/users", userRoutes);
 // --------------------Deployment--------------------------------
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
