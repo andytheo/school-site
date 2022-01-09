d974212fc4f739ef91be70c8ba440b31c11ad515
andytheo <igbeinkutuandrew@yahoo.com>
2022-01-09 14:09:43 -0500

    First deployment

--- server/server.js
+++ server/server.js
@@ -21,10 +21,10 @@ app.use("/api/users", userRoutes);
 // --------------------Deployment--------------------------------
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
