f8da27b1e87c2cbecf791361eca61fd437865837
andytheo <igbeinkutuandrew@yahoo.com>
2022-01-09 13:11:24 -0500

    redux

--- server/server.js
+++ server/server.js
@@ -3,21 +3,38 @@ const express = require("express");
 const nodes = require('./data/node');
 const userRoutes = require('./routes/userRoutes');
 const connectDb = require('./config/db');
+const path = require('path');
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 dotenv.config();
 
 const app = express();
 connectDb();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API running")
-})
 
 app.get("/api/nodes", (req, res) => {
   res.json(nodes)
 })
 
 app.use("/api/users", userRoutes);
+
+// --------------------Deployment--------------------------------
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running");
+  });
+}
+// -------------------------------------------------------
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000;
 
